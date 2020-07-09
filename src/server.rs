c6c3cfae1165e1e0bd6665e40e6179a915287fdc
José González Krause <josef@hackercat.ninja>
2020-07-09 23:20:33 +0200

    Added upload template and future functionality endpoints

--- src/server.rs
+++ src/server.rs
@@ -22,16 +22,47 @@ use actix_files as fs;
 #[allow(unused_variables)]
 #[actix_rt::main]
 pub async fn start(host: String, port: String, secure: bool, path: String) -> std::io::Result<()> {
+    let host_addr = format!("{}:{}", host, port);
+    let html_post = format_template(&host_addr);
+
     HttpServer::new(|| {
         App::new()
-            .service(fs::Files::new("/", ".").show_files_listing())
-            .route("/upload", web::get().to(index))
+            .route("/", web::get().to(index))
+            .route("/upl", web::get().to(upload))
+            .route("/cmd", web::get().to(cmd))
+            .service(fs::Files::new("/nav", ".").show_files_listing())
     })
-    .bind(format!("{}:{}", host, port))?
+    .bind(host_addr)?
     .run()
     .await
 }
 
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body(r#"
+    <p><a href="/upl">Upload</a></p>
+    <p><a href="/nav">Navigate</a></p>
+    <p><a href="/cmd">Command line</a></p>
+    "#)
+}
+
+async fn upload() -> impl Responder {
+    HttpResponse::Ok().body("Upload Template!")
+}
+
+async fn cmd() -> impl Responder {
+    HttpResponse::Ok().body("Cmd client!")
+}
+
+fn format_template(host: &str) -> String{
+    format!(r#"<html>
+    <head>
+           <title>Upload file</title>
+    </head>
+    <body>
+    <form enctype="multipart/form-data" action="http://{}/upload" method="post">
+        <input type="file" name="uploadfile" />
+        <input type="submit" value="upload" />
+    </form>
+    </body>
+    </html>"#, host)
 }
